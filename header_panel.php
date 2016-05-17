d07cccb0c8fb0b7233a16c76ba7ef24ca85bf2f6
Manas25 <manas.acharyya25@gmail.com>
2016-05-18 00:31:58 +0530

    2nd Commit: Header Panel Created

---
+++ header_panel.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/animate.css">
+	</head>
+
+	<body>
+		<div class="w3-container" style="height:100%;width:100%;position:fixed;top:0px;left:0px;">
+			
+			<div class="w3-container" style="padding-top:5px;padding-bottom:10px;margin-top:10px;">  
+				
+				<div class="w3-container w3-center">
+					<img src="images/header_panel/img1.jpg" class="w3-round" style="width:100%;max-height:200px;">
+					<div class="w3-row">
+						<div class="w3-col w3-container" style="width:5%;"> </div>
+
+						<div class="w3-rest w3-container w3-black w3-rightbar w3-border-red w3-hide-small" style="padding:5px;">
+							<label class="w3-text-white w3-large" style="font-family:tahoma;"> Ground Up To Sky High </label>
+						</div> 
+					</div>
+				</div>
+
+				<!-- FOR LARGE AND MEDIUM SCREENS !-->
+				<div class="w3-container w3-hide-small" style="margin-left:2%;margin-top:-13%">
+					<img src="images/header_panel/img2.jpg" class="w3-border w3-round" style="width:22%;min-height:100px;">
+				</div>
+
+				<!-- FOR SMALL SCREENS !-->
+				<div class="w3-container w3-hide-large w3-hide-medium" style="margin-left:2%;margin-top:-19%">
+					<img src="images/header_panel/img2.jpg" class="w3-border w3-round" style="width:22%;min-height:100px;">
+				</div>
+
+			</div>
+		</div>
+	</body>
+
+</html>
