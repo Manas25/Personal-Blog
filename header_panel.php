3e37e655a9325b88c302d474bbeb98f1719d55ff
Manas25 <manas.acharyya25@gmail.com>
2016-05-19 21:25:11 +0530

    Responsive Navigation Bar

--- header_panel.php
+++ header_panel.php
@@ -7,12 +7,24 @@
 		<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="css/style.css">
+
+		<script type="text/javascript" src="javascript/script.js"></script>
+
+		<!--
+		function navBarFunc() {
+		    var x = document.getElementById("nav");
+		    if (x.className.indexOf("w3-show") == -1) {
+		        x.className += " w3-show";
+		    } else { 
+		        x.className = x.className.replace(" w3-show", "");
+		    }
+		}
+		</script>!-->
 	</head>
 
 	<body>
-		<div class="w3-container" style="height:100%;width:100%;position:fixed;top:0px;left:0px;">
-			
-			<div class="w3-container" style="padding-top:5px;padding-bottom:10px;margin-top:10px;">  
+		<div class="w3-container" style="height:100%;width:100%;position:fixed;top:0px;left:0px;margin:0px;padding:0px;">
 				
 				<div class="w3-container w3-center">
 					<img src="images/header_panel/img1.jpg" class="w3-round" style="width:100%;max-height:200px;">
@@ -25,17 +37,57 @@
 					</div>
 				</div>
 
-				<!-- FOR LARGE AND MEDIUM SCREENS !-->
-				<div class="w3-container w3-hide-small" style="margin-left:2%;margin-top:-13%">
+				<!-- FOR LARGE SCREENS !-->
+				<div class="w3-container w3-hide-small w3-hide-medium" style="margin-top:-180px">
+					<img src="images/header_panel/img2.jpg" class="w3-border w3-round" style="width:22%;min-height:100px;">
+				</div>
+
+				<!-- FOR MEDIUM SCREENS !-->
+				<div class="w3-container w3-hide-large w3-hide-small" style="margin-top:-90px">
 					<img src="images/header_panel/img2.jpg" class="w3-border w3-round" style="width:22%;min-height:100px;">
 				</div>
 
 				<!-- FOR SMALL SCREENS !-->
-				<div class="w3-container w3-hide-large w3-hide-medium" style="margin-left:2%;margin-top:-19%">
+				<div class="w3-container w3-hide-large w3-hide-medium" style="margin-top:-50px">
 					<img src="images/header_panel/img2.jpg" class="w3-border w3-round" style="width:22%;min-height:100px;">
 				</div>
 
-			</div>
+				<!--- RESPONSIVE NAVIGATION BAR !-->
+				<div class="w3-row w3-container w3-center" style="margin-top:10px;">
+					<a href="#">
+						<div class="w3-quarter tablink w3-bottombar w3-border-red w3-padding w3-hover-light-grey">
+								<span class="glyphicon glyphicon-home"></span>
+								<a href="#" onclick="navBarFunc()" class="w3-hide-large w3-hide-medium w3-right">
+									<span class="w3-large w3-hover-text-red">☰</span>
+								</a>
+						</div>
+					</a>
+					<a href="#" class="w3-hide-small">
+						<div class="w3-quarter tablink w3-bottombar w3-border-red w3-padding w3-hover-light-grey">
+								<span class="glyphicon glyphicon-edit"></span>
+						</div>
+					</a>
+					<a href="#" class="w3-hide-small">
+						<div class="w3-quarter tablink w3-bottombar w3-border-red w3-padding w3-hover-light-grey">
+								<span class="glyphicon glyphicon-expand"></span>
+						</div>
+					</a>
+					<a href="#" class="w3-hide-small">
+						<div class="w3-quarter tablink w3-bottombar w3-border-red w3-padding w3-hover-light-grey">
+								<span class="glyphicon glyphicon-cog"></span>
+						</div>
+					</a>
+				</div>
+
+				<div id="nav" class="w3-container w3-hide w3-hide-large w3-hide-medium">
+					<ul class="w3-navbar w3-center-align w3-hover-light-grey">
+					    <li><a href="#"><span class="glyphicon glyphicon-edit"></span></a></li>
+					    <li><a href="#"><span class="glyphicon glyphicon-expand"></span></a></li>
+					    <li><a href="#"><span class="glyphicon glyphicon-cog"></span></a></li>
+					  </ul>
+				</div>
+
+				<!--                 RESPONSIVE NAVIGATION BAR              !-->
 		</div>
 	</body>
 
